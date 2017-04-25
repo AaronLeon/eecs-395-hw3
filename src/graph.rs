6561a475fa89c8162794670bfd44b1d1322fdddd
agamgupta <agamgupta.96@gmail.com>
2017-04-24 22:31:37 -0500

    fixed borrowing issues

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 use std::hash::Hash;
 
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Hash, PartialEq, Eq, Clone)]
 struct Node<T> where T: Hash + PartialEq + Eq {
     value: T,
     neighbors: Vec<Node<T>>,
@@ -12,21 +12,21 @@ pub struct Graph<T> where T: Hash + PartialEq + Eq {
 }
 
 
-impl<T> Graph<T> where T: Hash + PartialEq + Eq {
+impl<T> Graph<T> where T: Hash + PartialEq + Eq + Clone {
     fn new() -> Graph<T> {
         Graph {
             nodes: HashSet::new()
         }
     }
 
-    fn add(&self, new_node:Node<T>) {
-        &self.nodes.insert(new_node);
-    }
-
-    
+    fn add(&mut self, mut new_node:Node<T>) {
+        if !self.nodes.contains(&new_node){
+            self.nodes.insert(new_node);
+        }
+    }  
 }
 
-impl<T> Node<T> where T: Hash + PartialEq  + Eq {
+impl<T> Node<T> where T: Hash + PartialEq  + Eq + Clone {
     fn new(value:T, neighbors:Vec<Node<T>>) -> Node<T> {
         Node {
             value: value,
@@ -34,12 +34,19 @@ impl<T> Node<T> where T: Hash + PartialEq  + Eq {
         }
     }
 
-    fn add_neighbor(&self, new_neighbor:Node<T>) {
+    fn add_neighbor(&mut self, mut new_neighbor:Node<T>) {
+        let current_node: Node<T> = self.clone();
         if !&self.neighbors.contains(&new_neighbor) {
-            if !new_neighbor.neighbors.contains(&self) {
-                new_neighbor.neighbors.push(self);
-            }
+            // Add the node to the current node's edge list
             &self.neighbors.push(new_neighbor); 
+            // Then we should add the other side of the edge, by adding the current node in its neighbor's edge list
+            // We have to check if the neighbor exists in the graph, if it does then we add data to its edge list
+            // otherwise we add a node to the graph. 
+            // Graph::add(&new_neighbor);
+
+            if !new_neighbor.neighbors.contains(&self) {
+                new_neighbor.neighbors.push(current_node);
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod graph;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn make_graph(){
+    println!("Make the graph");
+    // Read the input from stdin line by line
+    // Split on whitespaces, and iterate over the data adding a node to the graph one at a time
 }
