f4c230b5422068528606f02c6c0333cfa322b723
Aaron Leon <aaronleon2018@u.northwestern.edu>
2017-04-26 05:00:36 -0500

    wip

--- src/graph.rs
+++ src/graph.rs
@@ -1,64 +1,177 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::hash::Hash;
-
-#[derive(Hash, PartialEq, Eq, Clone)]
-pub struct Node {
-    value: String,
-    neighbors: Vec<Node>,
-}
 
 pub struct Graph {
-    nodes: HashMap<String, Node>
+    pub nodes: HashMap<String, HashSet<String>>
 }
 
-
 impl Graph {
-    pub fn new() -> Graph {
-        let mut graph = Graph {
+    pub fn new() -> Self {
+        let graph = Graph {
             nodes: HashMap::new()
         };
         graph
     }
 
-    pub fn add(&mut self, value:String) {
-        let v = value.clone();
-        let new_neighbors = Vec::new();
-        let new_node:Node = Node::new(value, new_neighbors);
-        self.nodes.insert(v, new_node);
+    pub fn add_node(&mut self, value:&String) {
+        let neighbors = HashSet::new();
+        self.nodes.insert(value.to_owned(), neighbors);
     } 
 
-    pub fn get(&mut self, value:String) -> &mut Node {
-        match self.nodes.get_mut(&value) {
-            Some(node) => return node,
+    pub fn add_edge(&mut self, a:&String, b:&String) {
+        if !self.nodes.contains_key(a) {
+            self.add_node(a)
+        } else if !self.nodes.contains_key(b) {
+            self.add_node(b)
+        }
+        self.nodes.get_mut(a).unwrap().insert(b.to_owned());
+        self.nodes.get_mut(b).unwrap().insert(a.to_owned());
+    }
+
+    pub fn get_neighbors(&self, value:&String) -> &HashSet<String> {
+        match self.nodes.get(value) {
+            Some(neighbors) => return neighbors,
             None => panic!("no node found!"),
         }
     }
-}
 
-impl Node {
-    pub fn new(value:String, neighbors:Vec<Node>) -> Node {
-        let mut node = Node {
-            value: value,
-            neighbors: neighbors,
-        };
-        node
+    pub fn get_path(&self, a:&String, b:&String) -> Option<Vec<String>> {
+        let mut visited = HashSet::new();
+        let mut path = Vec::new();
+        let res = self.get_path_helper(a, b, &mut visited, &mut path);
+        println!("FOUND: {:?}", res);
+        res
+        //self.get_path_helper(a, b, &mut visited, &mut path)
     }
 
-    pub fn add_neighbor(&mut self, new_neighbor: &mut Node) {
-        let nn = new_neighbor.clone();
-        let current = self.clone();
+    fn get_path_helper(&self, a:&String, b:&String, visited:&mut HashSet<String>, path:&mut Vec<String>) -> Option<Vec<String>> {
+        visited.insert(a.to_owned());
+        if a == b {
+            println!("FOUND: {:?}", visited);
+            path.push(b.to_owned());
+            return Some(path.clone());
+        }
+        println!("{}", a);
+        println!("{:?}", visited);
+        if let Some(neighbors) = self.nodes.get(a) {
+            for neighbor in neighbors {
+                if !visited.contains(neighbor) {
+                    path.push(neighbor.to_owned());
+                    if let Some(res) = self.get_path_helper(neighbor, b, visited, path) {
+                        return Some(path.clone());
+                    }
+                }
+            }
+        }
+        None
+    }
+}
 
-        // Add the node to the current node's edge list
-        if !&self.neighbors.contains(new_neighbor) {
-            &self.neighbors.push(nn); 
-        }
-        // Stringhen we should add the other side of the edge, by adding the current node in its neighbor's edge list
-        // We have to check if the neighbor exists in the graph, if it does then we add data to its edge list
-        // otherwise we add a node to the graph. 
-        // Graph::add(&new_neighbor);
-        if new_neighbor.neighbors.contains(self) {
-            new_neighbor.neighbors.push(current);
-        }
+#[cfg(test)]
+mod graph_tests {
+    use super::Graph;
+    use std::io::Cursor;
+    use std::collections::{HashMap, HashSet};
+
+    #[test]
+    fn new_graph_is_empty_test() {
+        let g = Graph::new();
+        assert!(g.nodes.is_empty());
+    }
+
+    #[test]
+    fn add_node_creates_node_test() {
+        let mut g = Graph::new();
+        let value = "a".to_string();
+        g.add_node(&value);
+        assert!(g.nodes.contains_key(&value));
+        assert!(g.nodes.get(&value).unwrap().is_empty());
+    }
+
+    #[test]
+    fn get_neighbors_returns_neighbors_test() {
+        let mut g = Graph::new();
+        let value = "a".to_string();
+        let neighbor = "b".to_string();
+        g.add_node(&value);
+        g.nodes.get_mut(&value).unwrap().insert((&neighbor).to_owned());
+        assert!(g.get_neighbors(&value).contains(&neighbor));
+    }
+
+    #[test]
+    fn add_edge_appends_to_both_neighbors_test() {
+        let mut g = Graph::new();
+        let a = "a".to_string();
+        let b = "b".to_string();
+        g.add_node(&a);
+        g.add_edge(&a, &b);
+        assert!(g.get_neighbors(&a).contains(&b));
+        assert!(g.get_neighbors(&b).contains(&a));
+    }
+
+    #[test]
+    fn get_path_of_adjacent_nodes_test() {
+        let mut g = Graph::new();
+        let a = "a".to_string();
+        let b = "b".to_string();
+        let c = "c".to_string();
+        let d = "d".to_string();
+        let e = "e".to_string();
+        g.add_node(&a);
+        g.add_node(&b);
+        g.add_node(&c);
+        g.add_node(&d);
+        g.add_node(&e);
+        g.add_edge(&a, &b);
+        g.add_edge(&a, &d);
+        g.add_edge(&c, &e);
+        g.add_edge(&d, &b);
+        g.add_edge(&e, &d);
+
+        let expected1 = vec![a.to_owned(), b.to_owned(), d.to_owned()];
+        let expected2 = vec![a.to_owned(), d.to_owned()];
+        let path = g.get_path(&a, &c).unwrap();
+        print!("{:?}", path);
+        assert!(path == expected1 || path == expected2);
+    }
+
+    #[test]
+    fn get_path_of_distant_nodes_test() {
+        let mut g = Graph::new();
+        let a = "a".to_string();
+        let b = "b".to_string();
+        let c = "c".to_string();
+        let d = "d".to_string();
+        let e = "e".to_string();
+        g.add_node(&a);
+        g.add_node(&b);
+        g.add_node(&c);
+        g.add_node(&d);
+        g.add_node(&e);
+        g.add_edge(&a, &b);
+        g.add_edge(&a, &d);
+        g.add_edge(&c, &e);
+        g.add_edge(&d, &b);
+        g.add_edge(&e, &d);
+
+        let expected1 = vec![a.to_owned(), b.to_owned(), d.to_owned(), e.to_owned(), c.to_owned()];
+        let expected2 = vec![a.to_owned(), d.to_owned(), e.to_owned(), c.to_owned()];
+        let path = g.get_path(&a, &c).unwrap();
+        assert!(path == expected1 || path == expected2);
+    }
+
+    #[test]
+    fn get_path_of_linear_path_test() {
+
+    }
+
+    #[test]
+    fn get_path_of_same_node_test() {
+
+    }
+
+    #[test]
+    fn get_path_when_no_path_exists_test() {
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,50 @@
 use std::env;
 use std::fs::File;
 use std::io::{stdin, Read};
-use std::collections::{HashMap, HashSet};
 
 mod graph;
+use graph::Graph;
 
 fn main() {
-    let lines:Vec<String> = read_train_file();
+    let nodes = read_graph_file();
+    //let g = generate_graph(nodes);
+    let mut g = Graph::new();
+    let a = "a".to_string();
+    let b = "b".to_string();
+    let c = "c".to_string();
+    let d = "d".to_string();
+    let e = "e".to_string();
+    g.add_node(&a);
+    g.add_node(&b);
+    g.add_node(&c);
+    g.add_node(&d);
+    g.add_node(&e);
+    g.add_edge(&a, &b);
+    g.add_edge(&a, &d);
+    g.add_edge(&c, &e);
+    g.add_edge(&d, &b);
+    g.add_edge(&e, &d);
+
+    let expected1 = vec![a.to_owned(), b.to_owned(), d.to_owned()];
+    let expected2 = vec![a.to_owned(), d.to_owned()];
+    let path = g.get_path(&a, &c);
+    match path {
+        Some(p) => print!("{:?}", p),
+        None => print!("NONE!!"),
+    }
 }
 
-fn generate_graph(input_lines:Vec<String>) -> graph::Graph {
-    let mut g:graph::Graph = graph::Graph::new();
+fn generate_graph(nodes:Vec<Vec<String>>) -> graph::Graph {
+    let mut g:Graph = Graph::new();
 
-    input_lines.iter().map(|line| {
-        let mut n_iter = line.split_whitespace();
-        let first = n_iter.next();
-        g.add(first.unwrap().to_string());
-        for value in n_iter {
-            let neighbors = Vec::new();
-            let mut new_neighbor:graph::Node = graph::Node::new(value.to_string(), neighbors);
-            let current:&mut graph::Node = g.get(value.to_string());
-            current.add_neighbor(&mut new_neighbor);
+    for node in nodes.iter() {
+        let mut node_iter = node.iter();
+        let first = node_iter.next().unwrap();
+        g.add_node(&first);
+        for value in node_iter {
+            g.add_edge(&first, &value.to_string());
         }
-    });
+    };
 
     g
 }
@@ -37,23 +59,27 @@ fn read_input<R: Read>(mut reader: R) -> Vec<String> {
     result
 }
 
-fn read_train_file() -> Vec<String> {
+fn read_graph_file() -> Vec<Vec<String>> {
     let file_name = env::args().nth(1).unwrap();
     let mut f = File::open(file_name).unwrap();
     let mut buffer = String::new();
     f.read_to_string(&mut buffer).ok();
     // buffer
-    let result:Vec<String> = buffer.split('\n')
-        .map(|line| line.to_string())
-        .collect();
+    let mut result:Vec<Vec<String>> = Vec::new();
+
+    for line in buffer.split('\n') {
+        let mut temp:Vec<String> = Vec::new();
+        for token in line.split(' ') {
+            temp.push(token.to_string());
+        }
+        result.push(temp);
+    }
 
     result
 }
 
-fn make_graph(){
-    println!("Make the graph");
-    // Read the input from stdin line by line
-    // Split on whitespaces, and iterate over the data adding a node to the graph one at a time
-
+#[cfg(test)]
+mod path_finder_test {
+    use super::{read_graph_file, generate_graph};
 
 }
